c321e44cbc7777b40c27857138073c9cb82fc3f6
Akhil S <84123028+asalpineswift@users.noreply.github.com>
2026-08-21 14:52:05 +0530

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int[] res = new int[2];
+        for(int i=0; i<s.length(); i++){
+            //odd length
+            res = findPalindrome(i, i, res, s);
+            //even length
+            res = findPalindrome(i, i+1, res, s);
+            
+        }
+        return s.substring(res[0], res[0]+res[1]);
+    }
+
+    private int[] findPalindrome(int l, int r, int[] res, String s){
+        int resIdx = res[0];
+        int resLen = res[1];
+         while (l >= 0 && r < s.length() &&
+                   s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > resLen) {
+                    resIdx = l;
+                    resLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        return new int[]{resIdx, resLen};
+    }
+}
