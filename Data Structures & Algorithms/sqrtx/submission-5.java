d8d2aae1822100c233a2a26155a51aa6a6402f27
Akhil S <84123028+asalpineswift@users.noreply.github.com>
2026-09-20 18:55:34 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) { 
+        int l = 1, r = x, min = 1;
+        if (x < 1) return 0;
+
+        while (l<=r){
+            int m = l + (r-l)/2;
+            if( (long) m * m <= x){
+                min = m
+                l = m+1;
+            }else {
+                r = m-1;
+            }
+        }
+        return min;
+    }
+}
