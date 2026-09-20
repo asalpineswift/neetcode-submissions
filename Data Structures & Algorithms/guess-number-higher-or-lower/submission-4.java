e790ce50e8ce332de3884eb34832a333de71e4be
Akhil S <84123028+asalpineswift@users.noreply.github.com>
2026-09-20 11:16:36 +0530

    Add: guess-number-higher-or-lower - submission-4

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-4.java
@@ -0,0 +1,37 @@
+/**
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        // num -> number I choose
+        // 0 - Equal to the guess
+        // -1 - Guess is lesser
+        // 1 - Guess is greater
+        // range is from  1 to int value;
+
+        int low = 1;
+        int high = n;
+        int mid = low + (high - low) / 2;
+        int grace;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            grace = guess(mid);
+            if (grace == 0) {
+                break;
+            } else if (grace > 0) {
+                // guess is greater than mid
+                low = mid+1;
+            } else {
+                // guess is lower than mid
+                high = mid-1;
+            }
+        }
+        return mid;
+    }
+}
